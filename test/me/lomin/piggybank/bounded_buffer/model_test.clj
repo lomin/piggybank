66ecd2d89c245e22fc6630bcaca977fba669a4f9
lomin <happylomin@gmail.com>
2019-07-08 10:56:31 +0200

    Made the performance shortcut explicit.

--- test/me/lomin/piggybank/bounded_buffer/model_test.clj
+++ test/me/lomin/piggybank/bounded_buffer/model_test.clj
@@ -48,6 +48,12 @@
                    :interpreter intp/interpret-timeline
                    :universe    (spec/make-empty-universe spec/BUFFER-LENGTH)})))
 
+(defn performance-shortcut
+  "The last generated model finds the bug in the extreme programming challenge fourteen.
+   In order to receive fast feedback, we make sure, that the last model gets checked first."
+  [models]
+  (reverse models))
+
 (defn check-all [length keys]
   (reduce (fn [result model]
             (if (:property-violated result)
@@ -59,27 +65,68 @@
                      length
                      keys)))
           keys
-          (model/all-models 3)))
+          (performance-shortcut (model/all-models 3))))
 
 (deftest ^:slow-model bounded-buffer-model-test
-  (is (= {:buffer            [:X]
-          :occupied          0
-          :property-violated {:name     :there-must-be-no-deadlocks
-                              :timeline [[:consumer {:id 0, :notify 0}]
-                                         [:consumer {:id 1, :notify 1}]
+  (is (= {:buffer [:X]
+          :check-count 4219
+          :history '({:buffer [:X]
+                      :check-count 6
+                      :occupied 0
+                      :put-at 1
+                      :return :X
+                      :take-at 1
+                      :threads {1 :sleeping, 2 :sleeping}}
+                     {:buffer [:X]
+                      :check-count 5
+                      :occupied 0
+                      :put-at 1
+                      :return :X
+                      :take-at 1
+                      :threads {2 :sleeping}}
+                     {:buffer [:X]
+                      :check-count 4
+                      :occupied 1
+                      :put-at 1
+                      :take-at 0
+                      :threads {1 :sleeping, 2 :sleeping}}
+                     {:buffer [:X]
+                      :check-count 3
+                      :occupied 1
+                      :put-at 1
+                      :take-at 0
+                      :threads {1 :sleeping}}
+                     {:buffer [nil]
+                      :check-count 2
+                      :occupied 0
+                      :put-at 0
+                      :take-at 0
+                      :threads {0 :sleeping, 1 :sleeping}}
+                     {:buffer [nil]
+                      :check-count 1
+                      :occupied 0
+                      :put-at 0
+                      :take-at 0
+                      :threads {1 :sleeping}}
+                     {:buffer [nil], :occupied 0, :put-at 0, :take-at 0, :threads {}})
+          :max-check-count 33480783
+          :occupied 0
+          :property-violated {:name :there-must-be-no-deadlocks
+                              :timeline [[:consumer {:id 1, :notify 0}]
+                                         [:consumer {:id 0, :notify 0}]
                                          [:producer {:id 2, :notify 0}]
-                                         [:producer {:id 2, :notify 2}]
+                                         [:producer {:id 2, :notify 1}]
                                          [:consumer {:id 0, :notify 1}]
-                                         [:consumer {:id 0, :notify 2}]
-                                         [:consumer {:id 1, :notify 2}]]}
-          :put-at            1
-          :return            :X
-          :take-at           1
-          :threads           {0 :sleeping, 1 :sleeping, 2 :sleeping}}
+                                         [:consumer {:id 1, :notify 1}]
+                                         [:consumer {:id 0, :notify 2}]]}
+          :put-at 1
+          :return :X
+          :take-at 1
+          :threads {0 :sleeping, 1 :sleeping, 2 :sleeping}}
          (with-redefs [me.lomin.piggybank.bounded-buffer.spec/BUFFER-LENGTH 1]
-           (check-all 7 [:property-violated])))))
+           (check-all 7 nil)))))
 
-(deftest ^:unit ^:focus bounded-buffer-model-timeline-test
+(deftest ^:unit bounded-buffer-model-timeline-test
   (is (=* {:property-violated {:name     :there-must-be-no-deadlocks
                                :timeline [[:consumer {:id 0, :notify 0}]
                                           [:consumer {:id 1, :notify 1}]
